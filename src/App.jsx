c6c4bffb40778bf6c40ccfee22029574272dbca0
Moh Derajatul Ula <101638355+Derajatul@users.noreply.github.com>
2023-06-17 09:46:45 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className='container'>
+      <h1 className='text-center text-3xl font-bold lg:text-5xl'>
+        Hello world
+      </h1>
+    </div>
+  );
+};
+
+export default App;
