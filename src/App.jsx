7c5bc1ef989e170569f95e05869be9de8479d782
Derajatul <derajatul@gmail.com>
2023-07-12 14:23:47 +0700

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import MainLayout from "./Layout/MainLayout";
+import MainLayout from "./layout/MainLayout";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
 
